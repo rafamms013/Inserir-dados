bf50eb59993fe2e102988ad34fe2db4ff28afbe0
Rafael Montenegro M. da Silva <88899571+rafamms013@users.noreply.github.com>
2022-06-28 23:31:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("  ->Bem Vindo<-\n") 
+print("\n Digite suas informações \n")
+
+Nome_completo = input("\n Digite seu Nome completo: ")
+
+Idade = input("\n Digite sua idade: ")
+
+Grau_de_Escolaridade = input("\n Digite seu Grau de Escolaridade: ")
+
+Profissão = input("\n Digite sua Profissão: ")
+
+print("\n Suas Informções \n")
+
+
+print(Nome_completo)
+print(Idade)
+print(Grau_de_Escolaridade)
+print(Profissão)
